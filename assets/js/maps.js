c6658204e453c592b9bc9b7df3bc96b1ab49294e
DavW <Davidpmwalsh@protonmail.com>
2020-02-27 14:43:41 +0000

    added markers and title to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,108 @@
 
 
+function initMap(){
+      // Map options
+      var options = {
+        zoom:6,
+        center:{lat:53.270962,lng:-9.062691},
+        iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png'
+      }
+
+      // New map
+      var map = new google.maps.Map(document.getElementById('map'), options);
+
+     
+      // Add marker
+      var marker = new google.maps.Marker({
+        position:{lat:53.350140,lng:-6.266155},
+        map:map,
+        
+        
+      });
+
+       var infoWindow = new google.maps.InfoWindow({
+        content:'<h1>Athens, Greece</h1>'
+      });
+       marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+
+      addMarker({
+          coords:{lat:53.270962,lng:-9.062691},
+          iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+        content:'<h1> Galway, Ireland  </h1>'
+        });
+
+      addMarker({
+          coords:{lat:23.634501,lng:-102.552788},
+          iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+          content:'<h1> Mexico City, Mexico  </h1>'
+        });
+      addMarker({
+          coords:{lat:40.416775,lng:-3.703790},
+        iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+        content:'<h1> Madrid, Spain </h1>'
+        });
+      addMarker({
+          coords:{lat:40.712776,lng:-74.00597},
+          iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+          content:'<h1> Park Row, New York, NY 10007 </h1>'
+
+        });
+      addMarker({
+          coords:{lat: 53.349804,lng:-6.260310},
+        iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+        content:'<h1> O Connell Street, Palatine Square, Dublin </h1>'
+      });
+      addMarker({
+          coords:{lat:-15.826691,lng:-47.921822},
+        iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+        content:'<h1> Brasília Brasil</h1>'
+        });
+        addMarker({
+          coords:{lat:37.983810,lng:23.727539},
+        iconImage:'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+        content:'<h1> Athens, Greece </h1>'
+        });
+
+      // Add Marker Function
+      function addMarker(props){
+        var marker = new google.maps.Marker({
+          position:props.coords,
+          map:map,
+          //icon:props.iconImage
+           
+        });
+
+        //check for custom icon
+        if(props.iconImage){
+            //set icon image
+            marker.setIcon(props.iconImage);
+        }
+
+        //check content
+        if(props.content){
+            var infoWindow = new google.maps.InfoWindow({
+        content:props.content
+      });
+       marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+        }      
+    }
+}
+
+
+
+
+
+
+/*-------------------------------------------------------------------------------------------
+
+Trial & Error Maps //
+
+
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3.5,
@@ -30,7 +133,7 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*-------------------------------------------------
 
@@ -142,6 +245,5 @@ function initMap() {
                 imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
         }
-
-
-        ---------*/
+    }
+*/
